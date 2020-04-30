68effe9f2c244492756bfe97b6fd9ddd678fd945
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-04-30 11:24:00 -0400

    add auth to all admin pages

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_HTTP_BASIC_AUTH_USERNAME'], password: ENV['ADMIN_HTTP_BASIC_AUTH_PASSWORD']
   def index
   end
 
