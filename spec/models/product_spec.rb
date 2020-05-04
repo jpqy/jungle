a0b4f5df75c389d830f0cce0ef81cd67c63c4d25
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-05-04 19:20:09 -0400

    rspec for product validation

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+RSpec.describe Product, type: :model do
+  describe "Validations" do
+    category = Category.new(name: 'Test Category')
+    subject {
+      described_class.new(name: "Anything",
+                          price_cents: 100,
+                          quantity: 1,
+                          category: category)
+    }
+
+    it "is valid with filled attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a price" do
+      subject.price_cents = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a quantity" do
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a category" do
+      subject.category = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "should have an expect error" do
+      subject.category = nil
+      subject.save
+      puts subject.errors
+      expect(subject.errors.full_messages).to_not be_empty
+    end
+  end
+end
