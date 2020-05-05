fc29fb171d40f42daabedd54ef895d484f2658fb
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-05-05 14:23:41 -0400

    feature spec for nav to product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+require 'images'
+
+RSpec.feature "Visitor navigates from home page to product page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They can click on a product and go to the product page" do
+    visit root_path
+
+    # Wait for page to render the 10 products
+    expect(page).to have_css 'article.product', count: 10
+    product = Product.last
+    click_on product.name
+
+    # VERIFY
+    expect(page).to have_css 'section.products-show'
+  end
+end
