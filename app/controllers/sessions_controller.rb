bce6b341cff69626682106f2fc4b6a506de877af
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-05-05 18:35:43 -0400

    flash message on login fail

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
+      flash[:error] = "Login failed!"
       redirect_to '/login'
     end
   end
