c3de76a8aac257afafae95e3304115a91dd527a6
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-05-04 22:52:11 -0400

    downcase and strip email in User auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 8}
 
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by_email(email)
+    user = User.find_by_email(email.downcase.strip)
     if user.authenticate(password)
       return user;
     end
