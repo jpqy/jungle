f29ac73c4aef759bb1bc5bfd523ac68e75e15960
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-05-05 18:43:41 -0400

    test for incorrect login

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -21,4 +21,16 @@ RSpec.feature "Visitor can login", type: :feature, js: true do
     expect(page).to have_field "email"
     expect(page).to have_field "password"
   end
+
+  scenario "Incorrect login results in an alert" do
+    visit login_path
+    expect(page).to have_field "email"
+
+    # Fill in field with wrong credentials
+    fill_in 'email', with: "john@doe.com"
+    fill_in 'password', with: "wrongpass"
+    click_button 'Login'
+
+    expect(page).to have_css ".alert"
+  end
 end
